864b3313d90aaabf991f0201a4d45c8deaf7ba85
up2118329 <up2118329@myport.ac.uk>
2025-12-17 10:05:30 +0000

    Add imports for numpy and pandas in functions.py

--- py/py/functions.py
+++ py/py/functions.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+from typing import Tuple, Dict, List, Optional
 def load_star_data(path="data/star_classification.csv"):
     import pandas as pd
     return pd.read_csv(path)
